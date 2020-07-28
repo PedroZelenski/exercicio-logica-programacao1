064bc8e1525b73af797ab8605c2bc0f7d6b020a8
Zelenski <zelenski.contato@gmail.com>
2020-07-28 15:48:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercicio_logica_programacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service service = new Service();
+            service.CalcularDias();
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_logica_programacao1
+{
+    public class Service
+    {
+        public int Meses { get; set; }
+        public int Dias = 30;
+
+        public void CalcularDias()
+        {
+            Console.WriteLine("Digite o numero de meses: ");
+            Meses = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"O total de dias dentro de {Meses} meses é: {Meses * Dias}")
+;       }
+    }
+}
